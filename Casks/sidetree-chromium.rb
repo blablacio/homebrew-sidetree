6060d24ba98faad8c8fd842aae7b30d2a94093bc
SideTree BuildOps <sidetree-buildops@users.noreply.github.com>
2026-07-08 14:48:26 +0300

    Update sidetree-chromium to sidetree-chromium-150.0.7871.101.1

--- Casks/sidetree-chromium.rb
+++ Casks/sidetree-chromium.rb
@@ -1,6 +1,6 @@
 cask "sidetree-chromium" do
-  version "150.0.7871.47.11"
-  sha256 "2ef536c94d441a2c987d4e37174a452038d1a888a9f90923bbb6c0a7380da8ff"
+  version "150.0.7871.101.1"
+  sha256 "35930675bfcd64bed897c07eb1bc2912a8be64cdc77f3059d79ac22cd92a2393"
 
   url "https://github.com/blablacio/chromium/releases/download/sidetree-chromium-#{version}/chromium-sidetree-chromium-#{version}.zip",
       verified: "github.com/blablacio/chromium/"
