75b01630309ebf1347dbec4da7427ac650b4b788
SideTree BuildOps <sidetree-buildops@users.noreply.github.com>
2026-08-10 03:54:36 +0300

    Update sidetree-helium to sidetree-helium-macos-0.15.3.1.1

--- Casks/sidetree-helium.rb
+++ Casks/sidetree-helium.rb
@@ -1,6 +1,6 @@
 cask "sidetree-helium" do
-  version "0.14.9.1.1"
-  sha256 "93838169904ed13ecf828964663812cddd55dd318094a11c3d4908ee7edb0a6a"
+  version "0.15.3.1.1"
+  sha256 "abb7bac1e30a1a921e9f6f206f0845a8ba68dfbdb93d94f561b7fb7762f77ebf"
 
   url "https://github.com/blablacio/helium-macos/releases/download/sidetree-helium-macos-#{version}/helium-macos-sidetree-helium-macos-#{version}.zip",
       verified: "github.com/blablacio/helium-macos/"
